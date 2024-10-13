3c04b4e781043533edb8babe8d20132d9a746438
Zhuridochka <zhuridochka@gmail.com>
2024-10-13 18:53:45 +0300

    Add and adaptive Partners section

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84494c39a8b21eb383543cdbb0140a0e5e04a610bdc4e7def46cc8ae3e5231aa
-size 4925
+oid sha256:b4320696628ff1666f0891491f1e5ea68962e63925f93823945bedb2bd2ef979
+size 7228
